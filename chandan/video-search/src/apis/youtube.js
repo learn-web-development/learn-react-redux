ab6e447dc598d73b07b585b9230342075321a87d
Chandan <chandan.gupta@gmail.com>
2021-01-17 16:02:05 +0530

    Remove and retire old API keys

--- chandan/video-search/src/apis/youtube.js
+++ chandan/video-search/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBsO1-aflc--Kuqeb4bv64rQ-sb65MsVOE';
+const KEY = 'YOUR_YOUTUBE_API_KEY';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
