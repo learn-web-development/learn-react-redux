39292e1a40e62fa6d1da627726cd462a2d65e248
komal <kg681278@gmail.com>
2021-01-19 22:57:14 +0530

    Completed Accordion coding

--- komal/Accordion/src/components/Accordion.js
+++ komal/Accordion/src/components/Accordion.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React,{ useState }from 'react';
+
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index)
+  }                          
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : ''; 
     return (
       <React.Fragment key={item.title}>
         <div 
-          className='title active'
-          onClick={() => console.log('Title clicked', index)}
+          className={`title ${active}`} onClick={() => onTitleClick(index)}
         >
           <i className='dropdown icon'></i>
           {item.title}
         </div>
-        <div className='content active'>
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     )
   })
-  return <div className='ui styled accordion'>{renderedItems}</div>
+  return(
+    <div className='ui styled accordion'>
+      {renderedItems}
+    </div>
+  ) 
 }
 
 export default Accordion
