5658d5d88f60979fd41150ec4689e6fdc7386287
Rohit <rohitgta7@gmail.com>
2021-01-30 20:21:20 +0530

    Complete second redux project "Store-Design"

---
+++ rohit/Blog/src/reducers/postsReducer.js
@@ -0,0 +1,10 @@
+const postsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default postsReducer;
